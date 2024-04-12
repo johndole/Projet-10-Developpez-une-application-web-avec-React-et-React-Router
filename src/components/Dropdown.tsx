be2980f5ae2811f4b28117a8d4e8acf9419152f3
johndole <isildur64500@gmail.com>
2024-04-12 10:42:28 +0200

    create dropdown component

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,26 @@
+
+import { useState } from 'react';
+import '../styles/Dropdown.css';
+import { BsChevronUp, BsChevronDown } from "react-icons/bs";
+
+const Dropdown = ({ title, content, style }:any) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="dropdown" style={style}>
+      <div className="dropdown-header" onClick={toggleDropdown}>
+        <h3>{title}</h3>
+        <span>{isOpen ? <BsChevronDown size={30}/> : <BsChevronUp size={30}/>}</span>
+      </div>
+      {isOpen && <div className="dropdown-content">{content}</div>}
+    </div>
+  );
+};
+
+export default Dropdown
+
+
